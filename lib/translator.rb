dbe61126e5fe5ebf71665b6893a3ffccb13720d1
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-10-12 16:16:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,6 @@ end
 
 def get_japanese_emoticon(file_path, english_emoticon)
   load_library(file_path).each do |key, value| 
-    #binding.pry 
     if english_emoticon == value[:english]
       return value[:japanese]
     end 
